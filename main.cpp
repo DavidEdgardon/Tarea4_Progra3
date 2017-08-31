dfb6cd8ebf39f1fab1518469dcdfd341ce3c455e
David Edgardo Nuñez <davidedgardonu@unitec.edu>
2017-08-31 06:59:02 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,41 @@
 // queue::push/pop
 #include "Mascota.h"
 #include "Evaluador.h"
-#include <list>
 #include <iostream>
+#include <list>
 #include <fstream>
-#include <string.h>
 using namespace std;
-int TAMANO_REGISTRO = 14;
+
 //Escribe los datos del objeto mascota dado en un archivo binario con el nombre dado en la posicion dada
 //Nota: Se deben poder agregar varias mascotas en un solo archivo
+int tamano_registro = 14;
 void escribir(string nombre_archivo, Mascota*mascota, int posicion)
 {
-    ofstream out(nombre_archivo.c_str(),ios::in);
-    if(!out.is_open()){
+    ofstream out(nombre_archivo.c_str(),ios::in|ios::out);
+    if(!out.is_open())
+    {
         out.open(nombre_archivo.c_str());
     }
-
-    out.seekp(posicion*40);
+    out.seekp(posicion* tamano_registro);
     out.write((char*)&mascota->edad,4);
-    out.write(mascota->nombre.c_str(),36);
+    out.write(mascota->nombre.c_str(),10);
     out.close();
+}
 
-    /*FILE * archivo= new FILE(nombre_archivo);
+//Devuelve una mascota previamente escrita (con la funcion escribir()) en un archivo binario con nombre dado en la posicion dada
+//Nota: Se deben poder leer varias mascotas de un solo archivo
+Mascota* leer(string nombre_archivo, int posicion)
+{
+    int edad;
+    char nombre[10];
+    ifstream in(nombre_archivo.c_str());
+    in.seekg(posicion * tamano_registro);
+    in.read((char*)&edad,4);
+    in.read(nombre,10);
+    Mascota *m = new Mascota(edad,nombre);
+    return m;
+    
+     /*FILE * archivo= new FILE(nombre_archivo);
     archivo = fopen(nombre_archivo,"rb+");
     fseek(archivo,posicion,SEEK_SET);
     int size= sizeof(string);
@@ -35,36 +49,31 @@ void escribir(string nombre_archivo, Mascota*mascota, int posicion)
     archivo=fclose(nombre_archivo);*/
 }
 
-//Devuelve una mascota previamente escrita (con la funcion escribir()) en un archivo binario con nombre dado en la posicion dada
-//Nota: Se deben poder leer varias mascotas de un solo archivo
-Mascota* leer(string nombre_archivo, int posicion)
-{
-
-    Mascota *m = new Mascota(-1,"");
-    ifstream in(nombre_archivo.c_str());
-    in.seekg(posicion*40);
-    int edad;
-    char nombre[36];
-    in.read((char*)&edad,4);
-    in.read(nombre,36);
-    m->edad = edad;
-    m->nombre = nombre;
-    return m;}
-
 //Devuelve un vector que contenga todas las mascotas previamente escritas (con la funcion escribir()) en un archivo binario con nombre dado
 //Nota: El vector debe contener las mascotas ordenadas de acuerdo a la posicion en la que se escribieron
-vector<Mascota*> leerTodos(string nombre_archivo) {
-    for (int c = 0; c < nombre_archivo.length();c++) {
-        char nombre[10];
-        int edad;
-        ifstream in(nombre_archivo.c_str());
-        in.seekg(c * TAMANO_REGISTRO);
-        in.read(nombre, 10);
-        in.read((char *) &edad, 4);
-        vector<Mascota *> respuesta;
-        return respuesta;
+vector<Mascota*> leerTodos(string nombre_archivo)
+{
+    vector<Mascota*>respuesta;
+    Mascota * mascotaNueva;
+    ifstream in(nombre_archivo.c_str());
+    in.seekg(0,ios::end);
+    int tamano_bytes = in.tellg();
+    double cantidad_mascotas = tamano_bytes/tamano_registro;
+    int edad;
+    char nombre[10];
+
+    for(int i=0;i<cantidad_mascotas;i++)
+    {
+        in.seekg(i * tamano_registro);
+        in.read((char*)&edad,4);
+        in.read(nombre,10);
+        mascotaNueva = new Mascota(edad,nombre);
+        respuesta.push_back(mascotaNueva);
     }
+        in.close();
+    return respuesta;
 }
+
 int main ()
 {
     //Funcion evaluadora
